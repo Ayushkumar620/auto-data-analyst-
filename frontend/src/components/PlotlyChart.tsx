26445b0c7b7eff18b935667cdc44b1593aaf31f2
Ayushkumar620 <ayushkumar620@users.noreply.github.com>
2026-08-25 18:21:23 +0530

    Updated Changes

--- frontend/src/components/PlotlyChart.tsx
+++ frontend/src/components/PlotlyChart.tsx
@@ -14,11 +14,30 @@ export default function PlotlyChart({ data, layout }: ChartSpec) {
       ref.current.innerHTML = '';
       Plotly.newPlot(ref.current, data, {
         responsive: true,
+        autosizable: true,
         margin: { t: 24, r: 12, b: 36, l: 48 },
         ...layout,
       });
+
+      const handleResize = () => {
+        if (ref.current) {
+          Plotly.Plots.resize(ref.current);
+        }
+      };
+
+      const resizeObserver = new ResizeObserver(() => {
+        handleResize();
+      });
+
+      resizeObserver.observe(ref.current);
+      window.addEventListener('resize', handleResize);
+
+      return () => {
+        resizeObserver.disconnect();
+        window.removeEventListener('resize', handleResize);
+      };
     }
   }, [data, layout]);
 
-  return <div ref={ref} className="plotly-chart" />;
+  return <div ref={ref} className="plotly-chart" style={{ width: '100%', maxWidth: '100%', minWidth: 0 }} />;
 }
